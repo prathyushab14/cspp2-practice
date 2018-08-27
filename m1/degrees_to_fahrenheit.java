26e095e21b2d649a7484dc29a8ff380b6bd2b8c4
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-27 17:47:43 +0530

    cels

--- m1/degrees_to_fahrenheit.java
+++ m1/degrees_to_fahrenheit.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
 class Degrees_fahrenheit
 {
     public static void main(String args[])
     {
         int c,f;
-        System.out.println("enter the temperature in celsius");
-        Scanner in = new Scanner(System.in);
-        c = in.nextInt();
+        c = 180;
 
         f = (c * 9 / 5) + 32;
 
