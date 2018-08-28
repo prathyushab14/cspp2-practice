6b76fbe7dd7fe0a4950b92ae8d3db1152adf9732
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-28 18:44:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m2/Assignment-3/Solution.java
+++ m2/Assignment-3/Solution.java
@@ -1,27 +1,35 @@
 import java.util.Scanner;
-
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);      
+/** power of base **/
+final class Solution {
+    /**
+    *Do not modify this main function.
+    **/
+    private Solution() { }
+    /**
+    * @param args finding power
+    **/
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-        long result = power(base,exponent);
+        long result = power(base, exponent);
         System.out.println(result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
-	public static long power(final int base, final int exponent) {
-		if (exponent == 0) {
-			return 1;
-		} else if (exponent == 1) {
-			return base;
-		}
-		else {
-			return base * power(base, exponent - 1);
-		}
-	}
+    }
+    /**
+    *Need to write the rootsOfQuadraticEquation function.
+    **/
+    /**
+    * @param base baseis a variable
+    * @param height height is a variable
+    **/
+    public static long power(final int base, final int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else if (exponent == 1) {
+            return base;
+        }
+        else {
+            return base * power(base, exponent - 1);
+        }
+    }
 }
