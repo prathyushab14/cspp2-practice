e71ec7a3a45a6c889fbbfb3e617f0f201a4ea6d2
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-29 17:09:02 +0530

    sum

--- cspp2-practice/m3/SumOfNNumbers.java
+++ cspp2-practice/m3/SumOfNNumbers.java
@@ -4,7 +4,7 @@ final class sum{
 		int s = 0;
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		for(int i = 1;i<= n;i++){
+		for(int i = 1;i <= n;i++){
 			s = s + i;
 		}
 		System.out.println(s);
