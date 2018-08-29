992237c7a62760fad0fafb7d0e303ca28626a80d
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-29 17:02:40 +0530

    pattern

---
+++ cspp2-practice/m3/ConverttoWhileLoop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+final class iter{
+	public static void main(final String[] args){
+		int n = 2;
+		while(n <= 10){
+			System.out.println(n);
+			n = n+2;
+		}
+		System.out.println("GoodBye!");
+	}
+} 
---
+++ cspp2-practice/m3/SumOfNNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+final class sum{
+	public static void main(String[] args){
+		int s = 0;
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 1;i<= n;i++){
+			s = s + i;
+		}
+		System.out.println(s);
+	}
+}
