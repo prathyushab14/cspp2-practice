6792451f0981bbef2c0c44b49ef0d9283a07e620
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-29 14:14:30 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -23,12 +23,16 @@ final class Solution {
     *Need to write the gcd function and print the output.
     **/
     /**
-    * @param n1 n1 is a variable
-    * @param n2 n2 is a variable
+    * @param n11 n1 is a variable
+    * @param n21 n2 is a variable
     * @return gcd
     **/
     public static int gcd(final int n11, final int n21) {
         int r;
+        /**
+         * @param n1 n1
+         * @param n2 n2
+         */
         int n1 =n11, n2 =n21;
         while (n2 > 0) {
             r = n1 % n2;
