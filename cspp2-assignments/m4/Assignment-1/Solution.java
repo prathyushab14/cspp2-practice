32c02442ec30deb5930e1f0ffa5a2e63929ef673
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-30 12:14:12 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -9,13 +9,13 @@ public class Solution
 		int arr1[];
 		int max = 0;
 		arr1 = new int[n];
-		for(int i = 0; i < arr1.length; i++) {
+		for (int i = 0; i < arr1.length; i++) {
 			arr1[i] = sc.nextInt();
 		}
-		for(int j = 0; j < arr1.length; j++) {
-			if(arr1[j]>max){
+		for (int j = 0; j < arr1.length; j++) {
+			if (arr1[j] > max) {
 				max = arr1[j];
-			}else max = max;
-		}System.out.println(max);
+			} else max = max;
+		} System.out.println(max);
 	}
 }
