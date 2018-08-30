3f34186258b0af3d76ec4eedba3d883a0361888c
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-30 15:51:50 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,43 +1,50 @@
 import java.util.Scanner;
+/**
+*Do not modify this main function.
+**/
 final class Solution {
     /* Fill the main function to print resultant of addition of matrices*/
+    private Solution() { }
+    /**
+    * @param args addition of matrices
+    **/
     public static void main(final String[] args) {
-        /** main
-        *function
-        **/
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
-        int[][] arr1 = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                arr1[i][j] = sc.nextInt();
-            }
-        }
-        int c = sc.nextInt();
-        int d = sc.nextInt();
-        int[][] arr2 = new int[c][d];
-        for (int i = 0; i < c; i++) {
-            for (int j = 0; j < arr2[i].length; j++) {
-                arr2[i][j] = sc.nextInt();
-            }
-        }
-        if (m != c || n != d) {
-            System.out.println("not possible");
-        } else {
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < arr1[i].length; j++) {
-                    arr1[i][j] = arr1[i][j] + arr2[i][j];
-                }
-            }
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < arr1[i].length; j++) {
-                    System.out.print(arr1[i][j]);
-                    if (j == 0 || j == 1) {
-                        System.out.print(" ");
-                    }
-                } System.out.println();
-            }
+    /** main
+    *function
+    **/
+    Scanner sc = new Scanner(System.in);
+    int m = sc.nextInt();
+    int n = sc.nextInt();
+    int[][] arr1 = new int[m][n];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            arr1[i][j] = sc.nextInt();
         }
     }
+    int c = sc.nextInt();
+    int d = sc.nextInt();
+    int[][] arr2 = new int[c][d];
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < arr2[i].length; j++) {
+            arr2[i][j] = sc.nextInt();
+        }
+    }
+    if (m != c || n != d) {
+        System.out.println("not possible");
+    } else {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                arr1[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                System.out.print(arr1[i][j]);
+                if (j == 0 || j == 1) {
+                    System.out.print(" ");
+                }
+            } System.out.println();
+        }
+    }
+}
 }
