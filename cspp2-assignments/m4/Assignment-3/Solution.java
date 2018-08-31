8c9e9067755ef27180635e3fd00fad2697ec2c73
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-31 16:21:53 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -12,20 +12,23 @@ public class Solution
 	{
 		Scanner sc=new Scanner(System.in);
 		int n= sc.nextInt();
-		for(int i=0;i<=n;i++){
+		sc.nextLine();
+		for(int i=0;i<n;i++){
 			String s=sc.nextLine();
 			String res=binaryToDecimal(s);//Write binaryToDecimal function
 			System.out.println(res);
 		}
 	}
 	public static String binaryToDecimal(String s){
-		double sum=0;
-		for(int i=0,j=s.length()-1;i<s.length();i++,j--){
-			int v = s.charAt(i);
-			if(v == 1){
-                sum = sum + Math.pow(2,j);
-            }
-		}return Double.toString(sum);
+		int sum=0;
+		int j = s.length()-1;
+		for(int i=0;i<s.length();i++){
+			int v = Character.getNumericValue(s.charAt(i));
+			// if(v == 1){
+                sum = sum + (int)Math.pow(2,j)*v;
+            // }
+                j--;
+		}return Integer.toString(sum);
 	}
 
 }
