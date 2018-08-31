acd7379ae8b3fd7f17494d14377d24db148db698
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-31 16:33:35 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -1,34 +1,47 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
 import java.util.Scanner;
+/**
+* { item_description }
+**/
 import java.lang.Math;
-public class Solution
-{/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		sc.nextLine();
-		for(int i=0;i<n;i++){
-			String s=sc.nextLine();
-			String res=binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	public static String binaryToDecimal(String s){
-		int sum=0;
-		int j = s.length()-1;
-		for(int i=0;i<s.length();i++){
-			int v = Character.getNumericValue(s.charAt(i));
-			// if(v == 1){
-                sum = sum + (int)Math.pow(2,j)*v;
-            // }
+/**
+* Class for solution.
+**/
+public class Solution {
+/**
+*Do not modify this main function.
+**/
+    private Solution() { }
+    /**
+    * @param args  The arguments.
+    **/
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for(int i = 0; i < n; i++){
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s);//Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
+    /**
+     * { function_description }
+     *
+     * @param s { parameter_description }
+     *
+     * @return sum { description_of_the_return_value }
+     */
+    public static String binaryToDecimal(String s){
+        int sum = 0;
+        /**
+         * { var_description }
+         */
+        int j = s.length() - 1;
+        for (int i = 0; i < s.length(); i++) {
+            int v = Character.getNumericValue(s.charAt(i));
+                sum = sum + (int) Math.pow(2,j)*v;
                 j--;
-		}return Integer.toString(sum);
-	}
-
+        } return Integer.toString(sum);
+    }
 }
