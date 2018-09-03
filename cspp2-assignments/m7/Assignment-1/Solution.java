2879a858261085271237c5a1a303d093a2859264
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-03 18:52:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -25,7 +25,8 @@ final class InputValidator {
         int length = a.length();
         if (length >= six) {
             return true;
-        } return false;
+        } 
+        return false;
     }
 }
 /**
