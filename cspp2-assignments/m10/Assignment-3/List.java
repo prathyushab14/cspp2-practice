530ba1445c2027194da29fbcca6e587362da4493
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-06 12:28:43 +0530

    "check_style_score -> 5 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-3/List.java
+++ cspp2-assignments/m10/Assignment-3/List.java
@@ -158,7 +158,7 @@ public class List<E> {
      */
     public boolean contains(E item) {
 		//Write logic for contains method
-		for (int i = 0; i < list.length; i++) {
+		for (int i = 0; i < size; i++) {
 			if (list[i] == item) {
 				return true;
 			}
