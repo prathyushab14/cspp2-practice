4befaf8acf018ff0c7a5eb42f5650c612ed1bb41
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-17 11:59:58 +0530

    "check_style_score -> 6 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m20/Assignment-1/Solution.java
+++ cspp2-assignments/m20/Assignment-1/Solution.java
@@ -256,7 +256,7 @@ public final class Solution {
         for (int i = 0; i < q; i++) {
             String ques = scan.nextLine();
             String[] toke = ques.split(":");
-            if (toke.length < 5) {
+            if (toke[0].equals("") || toke.length < 5) {
         	    System.out.println("Error! Malformed question");
         	    return;
         	}
