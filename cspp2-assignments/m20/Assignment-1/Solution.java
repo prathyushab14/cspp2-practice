78cc923739a66da683315434d1c630f68275a6db
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-17 23:35:04 +0530

    "check_style_score -> 13 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m20/Assignment-1/Solution.java
+++ cspp2-assignments/m20/Assignment-1/Solution.java
@@ -212,7 +212,7 @@ class Quiz {
                 + questions[i].getMaxMarks() + "\n";
                 sum += questions[i].getMaxMarks();
             } else {
-                s += " Wrong Answer! - Penalty: " 
+                s += " Wrong Answer! - Penalty: "
                 + questions[i].getPenalty() + "\n";
                 sum += questions[i].getPenalty();
             }
@@ -320,13 +320,13 @@ public final class Solution {
             }
             String[] choi = toke[one].split(",");
             if (choi.length < two) {
-                System.out.println(toke[0] + " does not " 
+                System.out.println(toke[0] + " does not "
                     + "have enough answer choices");
                 flag = false;
                 return;
             }
             if (Integer.parseInt(toke[two]) > four) {
-                System.out.println("Error! Correct answer choice" 
+                System.out.println("Error! Correct answer choice"
                     + " number is out of range for " + toke[0]);
                 flag = false;
                 return;
