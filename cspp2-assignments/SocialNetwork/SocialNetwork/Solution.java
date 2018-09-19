b7b82089b120fa9c1f957e6f87a5e616d9334483
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-19 23:14:24 +0530

    soc

--- cspp2-assignments/SocialNetwork/SocialNetwork/Solution.java
+++ cspp2-assignments/SocialNetwork/SocialNetwork/Solution.java
@@ -1,13 +1,21 @@
-import java.util.*;
+import java.io.*;
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.util.Arrays;
 class User {
 	String name;
-	String[] followers;
+	String[] followers = new String[20];
 	User() {
 
 	}
 	User(String name1, String[] follow) {
 		this.name = name;
 		this.followers = follow;
+	}
+    public void addconnector(String follow) {
+		String[] data = getFoll();
+		followers = Arrays.copyOf(data, followers.length + 1);
+		followers[followers.length - 1] = follow;
 	}
 	public String getName() {
 		return name;
@@ -24,16 +32,25 @@ class Social {
 		size = 0;
     }
 	public void getConnections(String name) {
+		String[] connect = new String[20];
+		boolean x = false;
 		for (int i = 0; i < size; i++) {
 			if (users[i].getName().equals(name)) {
-				System.out.println(users[i].getFoll());
+				connect = users[i].getFoll();
+				x = true;
 			}
+		}
+		if (x) {
+            System.out.println(Arrays.toString(connect));
+		} else {
+			System.out.println("Not a user in Network");
 		}
 	}
     public void addUser(User u1) {
     	User t = new User();
-    	for (int i = 0; i < size; i++) {
-            for (int j = i+1; j < size; j++) {
+    	users[size++] = u1;
+    	for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
     			if (users[i].getName().compareTo(users[j].getName()) > 0) {
     				t = users[i];
     				users[i] = users[j];
@@ -42,23 +59,51 @@ class Social {
     		}
     	}
     }
-	public void addConnections(User u) {
-		boolean y = false;
+    public void network() {
+    	int i = 0;
+    	for (i = 0; i < size - 1; i++) {
+    		System.out.println(users[i].getName()+": "+Arrays.toString(users[i].getFoll())+", ");
+    	}
+    	System.out.println(users[i].getName()+": "+Arrays.toString(users[i].getFoll()));
+    }
+	public void addConnections(String name, String foll) {
 		for (int i = 0; i < size; i++) {
-			if (users[i].getName().equals(u.name)) {
-				for (int j = 0; j < size; j++) {
-                    users[i].getFoll().add(u[j].getFoll());
-                    y = true;
+			if (users[i].getName().equals(name)) {
+                users[i].addconnector(foll);
+                break;
+			}
+		}
+    }
+    public void getcommonconnections(String name1, String name2) {
+    	String[] f = new String[20];
+    	String[] l = new String[20];
+    	String[] co = new String[20];
+    	int c = 0;
+    		for (int i = 0; i < size; i++) {
+			if (users[i].getName().equals(name1)) {
+				f = users[i].getFoll();
+
+			}
+		}
+		for (int j = 0; j < size; j++) {
+			if (users[j].getName().equals(name2)) {
+				l = users[j].getFoll();
+			}
+		}
+		for (int i = 0; i < f.length; i++) {
+			for (int j = 0; j < l.length; j++) {
+				if (f[i].equals(l[j])) {
+					co[c++] = f[i];
 				}
-		    }
-        }
-        if (!y) {
-        	users.add(u);
-        }
-	}
-}
-class Solution {
-	Solution() {
+			}
+		}
+		String[] commonlist1 = new String[c];
+		System.arraycopy(co, 0, commonlist1, 0, c);
+		System.out.println(Arrays.toString(commonlist1));
+		}
+    }
+public class Solution {
+	   Solution() {
 
 	}
 	public static void main(String[] args) {
@@ -72,8 +117,9 @@ class Solution {
         		int no = Integer.parseInt(tokens[1]);
         		for (int i = 0; i < no; i++) {
         			String s = sc.nextLine();
-        			String u = s.split(" is connected to ");
-        			String fl = u[1].replace(" "," ").replace(".","").split(",");
+        			String[] u = s.split(" is connected to ");
+        			String[] fl = u[1].replace(" ","").replace(".","").split(",");
+        			System.out.println(fl[2]);
         			so.addUser(new User(u[0], fl));
         		}
         		break;
