c8875043b3320940402969bd537e59cf8be5fbab
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-22 11:36:20 +0530

    "check_style_score -> 7 of 14 passed. style score: 0/1 "

--- cspp2-assignments/m25/Assignment-1/TodoistMain.java
+++ cspp2-assignments/m25/Assignment-1/TodoistMain.java
@@ -103,7 +103,7 @@ class Todoist {
 		Task tk = new Task();
 		String str ="";
 		for (int i = 0; i < size; i++) {
-			str += tasks[i].getTitle()+", "+tasks[i].getName()+", "+tasks[i].getTime()+", "+tasks[i].getImp()+", "+tasks[i].getUrg()+", "+tasks[i].getStatus();
+			str += tasks[i].getTitle()+", "+tasks[i].getName()+", "+tasks[i].getTime()+", "+tasks[i].getImp()+", "+tasks[i].getUrg()+", "+tasks[i].getStatus()+"\n";
 		}
 		return str;
 	}
