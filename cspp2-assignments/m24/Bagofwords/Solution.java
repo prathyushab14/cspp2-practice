5f06141611058c20355c67fa3984c689f7992cbd
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-23 15:10:25 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m24/Bagofwords/Solution.java
+++ cspp2-assignments/m24/Bagofwords/Solution.java
@@ -1,121 +1,152 @@
-import java.util.*;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import java.lang.Math.*;
-import java.io.*;
-class Doc {
-    String t1;
-    String t2;
-    Doc() {
-        t1 = "";
-        t2 = "";
+import java.util.Scanner;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+/**this class is to maintain.
+*complete details of two files.
+*/
+class Data {
+    /** this is an empty constructor.
+    */
+    Data() {
     }
-    public static String DocToString(final File f) {
-        String fileToString = "";
+    /**this method is to convert the.
+    *file document text to string
+    *@param file File
+    *@return str returns string of that text.
+    */
+    public static String toText(final File file) {
+        String str = "";
         try {
-            Scanner s = new Scanner(new FileReader(f));
-            StringBuilder sb = new StringBuilder();
-            while (s.hasNext()) {
-                sb.append(s.next());
-                sb.append(" ");
+            Scanner input = new Scanner(new FileReader(file));
+            StringBuilder text = new StringBuilder();
+            while (input.hasNext()) {
+                text.append(input.next());
+                text.append(" ");
             }
-            s.close();
-            fileToString = sb.toString();
+            input.close();
+            str = text.toString();
         } catch (FileNotFoundException e) {
-            System.out.println("no file");
+            System.out.println("No file");
         }
-        return fileToString;
+        return str;
     }
-    public static Map removewords(final String txt) {
-        String w = "";
-        Pattern p = Pattern.compile("[^0-9_.,]");
-        Matcher match = p.matcher(txt);
-        while (match.find()) {
-            w += match.group();
-        }
-        w = w.toLowerCase();
-        String[] words = w.split(" ");
-        Map <String, Integer> map = new HashMap<>();
-        for (int i = 0; i  < words.length; i++) {
-            if (!map.containsKey(words[i])) {
-                map.put(words[i], 1);
+    /**
+     * to remove the unwanted characters.
+     *
+     * @param      text  The text
+     *
+     * @return map which contains
+     * frequency of words.
+     */
+    public Map remove(final String text) {
+        text.toLowerCase();
+        text.replaceAll("[0-9_]", "");
+        String[] words = text.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        for (String element : words) {
+         if (element.length() > 0) {
+            if (!(map.containsKey(element))) {
+                map.put(element, 1);
             } else {
-                map.put(words[i], map.get(words[i]) + 1);
+                map.put(element, map.get(element) + 1);
             }
         }
+    }
         return map;
     }
-    public static int compare(final String string1, final String string2) {
-        float n = 0;
-        double d = 0;
-        float fSum = 0;
-        float sSum = 0;
-        final double hun = 100D;
-        final int h = 100;
-        final int two = 2;
-        Map <String, Integer> firstMap = removewords(string1);
-       Map <String, Integer> secondMap = removewords(string2);
-       for (String inmapOne : firstMap.keySet()) {
-           for (String inmapTwo : secondMap.keySet()) {
-               if (inmapOne.equals(inmapTwo)) {
-                   n += firstMap.get(inmapOne) * secondMap.get(inmapTwo);
-               }
-           }
-       }
-       for (String inmapOne : firstMap.keySet()) {
-           fSum += Math.pow(firstMap.get(inmapOne), two) ;
-       }
-       for (String inmapTwo : secondMap.keySet()) {
-           sSum += Math.pow(secondMap.get(inmapTwo), two);
-       }
-       d = Math.sqrt(fSum) * Math.sqrt(sSum);
-       double output = (n / d) * h;
-       return  (int) ((output * hun) / hun) ;
+    /**this method is to give the.
+     *document distance.
+     *@param textOne first file string
+     *@param textTwo second file string
+     *@return document distance
+     */
+
+    public int similarity(final String textOne, final String textTwo) {
+        double numerator = 0;
+        double denominator = 1;
+        double sumOne = 0;
+        double sumTwo = 0;
+        final int hundred = 100;
+        Map<String, Integer> mapOne = remove(textOne);
+        Map<String, Integer> mapTwo = remove(textTwo);
+        for (String element: mapOne.keySet()) {
+            for (String item: mapTwo.keySet()) {
+                if (element.equals(item)) {
+                    numerator += mapOne.get(element) * mapTwo.get(item);
+                }
+            }
+        }
+
+        for (String word: mapOne.keySet()) {
+            sumOne += mapOne.get(word) * mapOne.get(word);
+        }
+        for (String word: mapTwo.keySet()) {
+            sumTwo += mapTwo.get(word) * mapTwo.get(word);
+        }
+        denominator = Math.sqrt(sumOne) * Math.sqrt(sumTwo);
+        double documentDistance = (
+            (numerator / denominator) * hundred);
+        return (int) (documentDistance);
     }
 }
-/**
- * Class for solution.
- */
-class Solution {
-    /**
-     * Constructs the object.
-     */
+/** this is the solution class.
+*/
+public final class Solution {
+    /** an empty constructor.
+    */
     private Solution() {
 
     }
     /**
-     * main function.
+     * this is main method.
      *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        try {
-        Doc d = new Doc();
-        String path;
+        try  {
         Scanner scan = new Scanner(System.in);
-        path = scan.nextLine();
-        File folder = new File(path);
-        File[] list = folder.listFiles();
-        int length = list.length;
-        int[][] matrix = new int[length][length];
+        String input = scan.nextLine();
+        File files = new File(input);
+        Data obj = new Data();
+        File[] fileList = files.listFiles();
+        int length = fileList.length;
+        int maxValue = 0;
+        final int hundred = 100;
+        String result = "";
+        int[][] fileMatrix = new int[length][length];
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
-                matrix[i][j] = Doc.compare(Doc.DocToString(list[i]),
-                    Doc.DocToString(list[j]));
+                if (i == j) {
+                    fileMatrix[i][j] = hundred;
+                } else {
+                    fileMatrix[i][j] = obj.similarity(
+                        obj.toText(fileList[i]), obj.toText(fileList[j]));
+                    if (maxValue < fileMatrix[i][j]) {
+                        maxValue = fileMatrix[i][j];
+                        result = "Maximum similarity is between "
+                        + fileList[i].getName() + " and "
+                        + fileList[j].getName();
+                    }
+                }
             }
         }
         System.out.print("      \t");
-        for (int i = 0; i < list.length - 1; i++) {
-            System.out.print("\t" + list[i].getName());
+        for (int i = 0; i < length - 1; i++) {
+            System.out.print("\t" + fileList[i].getName());
         }
-        System.out.println("\t" + list[length - 1].getName());
+        System.out.println("\t" + fileList[length - 1].getName());
         for (int i = 0; i < length; i++) {
-            System.out.print(list[i].getName() + "\t");
+            System.out.print(fileList[i].getName() + "\t");
             for (int j = 0; j < length; j++) {
-                System.out.print(matrix[i][j] + "\t\t");
+                    System.out.print(fileMatrix[i][j] + "\t\t");
             }
             System.out.println();
         }
+     System.out.println(result);
     } catch (NoSuchElementException e) {
         System.out.println("empty directory");
     }
