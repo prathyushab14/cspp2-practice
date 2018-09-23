40e89774b12a9ca30fc3e674858fe74f1d8c76fa
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-23 14:52:01 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m24/Bagofwords/Solution.java
+++ cspp2-assignments/m24/Bagofwords/Solution.java
@@ -98,7 +98,7 @@ class Solution {
         for (int i = 0; i < length; i++) {
             System.out.print(list[i].getName() + "\t");
             for (int j = 0; j < length; j++) {
-                System.out.print(matrix[i][j] + "        ");
+                System.out.print(matrix[i][j] + "       ");
             }
             System.out.println();
         }
